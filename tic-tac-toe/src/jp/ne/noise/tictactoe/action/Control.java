bc8f5d22299f8df0e703acd8aa5d53b35e2fc150
Noise <noise@e-mail.jp>
2015-03-26 14:44:21 +0900

    Control.java Display.java

--- tic-tac-toe/src/jp/ne/noise/tictactoe/action/Control.java
+++ tic-tac-toe/src/jp/ne/noise/tictactoe/action/Control.java
@@ -1,5 +1,11 @@
 package jp.ne.noise.tictactoe.action;
 
-public class Control {
+import jp.ne.noise.tictactoe.model.Board;
+import jp.ne.noise.tictactoe.view.Display;
 
+public class Control {
+	public static void main(String[] args) {
+		Display display = new Display();
+		Board board = new Board();
+	}
 }
--- tic-tac-toe/src/jp/ne/noise/tictactoe/view/Display.java
+++ tic-tac-toe/src/jp/ne/noise/tictactoe/view/Display.java
@@ -2,4 +2,10 @@ package jp.ne.noise.tictactoe.view;
 
 public class Display {
 
+	public Display() {
+		System.out.println("*************************");
+		System.out.println("*      Tic-tac-toe      *");
+		System.out.println("*************************");
+	}
+
 }

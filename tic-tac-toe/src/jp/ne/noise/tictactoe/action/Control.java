9e3c4e7549f256aac21b92ff41115785868b168b
Noise <noise@e-mail.jp>
2015-03-30 09:33:42 +0900

    20150330

--- tic-tac-toe/src/jp/ne/noise/tictactoe/action/Control.java
+++ tic-tac-toe/src/jp/ne/noise/tictactoe/action/Control.java
@@ -1,5 +1,9 @@
 package jp.ne.noise.tictactoe.action;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import jp.ne.noise.tictactoe.model.Board;
 import jp.ne.noise.tictactoe.view.Display;
 
@@ -7,5 +11,17 @@ public class Control {
 	public static void main(String[] args) {
 		Display display = new Display();
 		Board board = new Board();
+
+		display.init();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			reader.readLine();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		System.out.println("Start");
 	}
 }
--- tic-tac-toe/src/jp/ne/noise/tictactoe/view/Display.java
+++ tic-tac-toe/src/jp/ne/noise/tictactoe/view/Display.java
@@ -3,9 +3,13 @@ package jp.ne.noise.tictactoe.view;
 public class Display {
 
 	public Display() {
-		System.out.println("*************************");
-		System.out.println("*      Tic-tac-toe      *");
-		System.out.println("*************************");
+		System.out.println("*********************************");
+		System.out.println("*          Tic-tac-toe          *");
+		System.out.println("*********************************");
+	}
+
+	public void init() {
+		System.out.println("Press Enter Key.");
 	}
 
 }
